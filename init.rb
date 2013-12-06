6460e1e5f3d83571d9de5fa591d0dfbb1f0ea383
Txinto Vaz <txinto@gmail.com>
2013-12-06 19:00:18 +0000

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :ucanca do
+  name 'Ucanca plugin'
+  author 'Txinto Vaz'
+  description 'uCANca implementation as Redmine plugin (this is a test)'
+  version '0.0.1'
+  url 'https://github.com/txinto/uCANca_rm'
+  author_url 'http://gatatac.org'
+end
